7999ec1fdd1356e94f572cab9c54a9b0368e050b
Oliver <oliver@oliver>
2012-05-14 12:54:17 +1000

    Added new sources of a new MINC png extractor

---
+++ src/c/png_creator.c
@@ -0,0 +1,87 @@
+#include "minc_extract_png.h"
+
+void		set_grayscale(png_byte *ptr, float val)
+{
+  ptr[0] = (png_byte)((int)val & 0xFF);
+  ptr[1] = (png_byte)((int)val >> 8);
+}
+
+void		get_width_height(int *height, int *width, int current_dimension, t_vol *volume)
+{
+  if (current_dimension == X)
+    {
+      *height = volume->size[Y];
+      *width = volume->size[Z];
+    }
+  else if (current_dimension == Y)
+    {
+      *height = volume->size[X];
+      *width = volume->size[Z];
+    }
+  else
+    {
+      *height = volume->size[X];
+      *width = volume->size[Y];
+    }
+}
+
+
+void            print_png(double *hyperslab, t_vol *volume, int current_dimension,
+                          unsigned int current_slice, int width, int height)
+{
+  png_infop	info_ptr;
+  png_structp	png_ptr;
+  png_bytep	row;
+  png_text	title_text;
+  FILE		*png_to_write;
+  int		x;
+  int		y;
+  char		*name;
+
+  name = NULL;
+  name = malloc(21 * sizeof(*name));
+  // create the name of the future png
+  sprintf(name, "./png/%i-%i.png", current_dimension, current_slice);
+  // open the png file
+  png_to_write = fopen(name, "wb");
+  // create the png_ptr
+  png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  // create a pointer for the png info
+  info_ptr = png_create_info_struct(png_ptr);
+  setjmp(png_jmpbuf(png_ptr));
+  // init the png type and the header
+  png_init_io(png_ptr, png_to_write);
+  png_set_IHDR(png_ptr, info_ptr, width, height, 16,
+	       PNG_COLOR_TYPE_GRAY,
+	       PNG_INTERLACE_NONE,
+	       PNG_COMPRESSION_TYPE_BASE,
+	       PNG_FILTER_TYPE_BASE); // 16 = number of bit - ex: image 16 bit
+  // create the title and set the compression level of the future png
+  title_text.compression = PNG_TEXT_COMPRESSION_NONE;
+  title_text.key = "Title";
+  title_text.text = "Hello";
+  // settings and writing title and info
+  png_set_text(png_ptr, info_ptr, &title_text, 1);
+  png_write_info(png_ptr, info_ptr);
+  // memory allocation for a row of pixels
+  row = (png_bytep)malloc(2 * width * sizeof(png_byte)); // 2 = 2 byte per pixel
+  y = 0;
+  // loop every pixels
+  while (y < height)
+    {
+      x = 0;
+      while (x < width)
+	{
+	  set_grayscale(&row[(x * 2)], (float)hyperslab[(y * width) + x]);
+	  x++;
+	}
+      png_write_row(png_ptr, row);
+      y++;
+    }
+  // writing and finishig all the pnglib stuff
+  png_write_end(png_ptr, NULL);
+  fclose(png_to_write);
+  png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+  png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
+  free(row);
+}
---
+++ src/c/thread_pool.c
@@ -0,0 +1,140 @@
+#include "minc_extract_png.h"
+
+void		*worker_start(void *pool)
+{
+  t_queue	*task;
+  t_thread_pool	*p;
+  
+  p = (t_thread_pool*)pool;
+  while (1)
+    {
+      task = NULL;
+      // lock mutex to avoid concurrent access
+      pthread_mutex_lock(&p->lock);
+      if (p->tasks_to_do == 0)
+	pthread_cond_wait(&(p->condvar), &(p->lock));
+      // get the task and delete the task from the queue
+      if (p->first != NULL)
+	{
+	  task = p->first;
+	  p->first = p->first->next;
+	  p->tasks_to_do--;
+	}
+      // unlock the mutex locked before
+      pthread_mutex_unlock(&(p->lock));
+      if (task != NULL)
+	{
+	  (*(task->function))(task->argument);
+	  free(task);
+	}
+    }
+  return (NULL);
+}
+
+void		thread_pool_add_task(void *(*function)(void *), void *args, t_thread_pool *p)
+{
+  t_queue	*tmp;
+  
+  pthread_mutex_lock(&(p->lock));
+  tmp = p->first;
+  if (p->first != NULL)
+    {
+      while (tmp->next != NULL)
+	tmp = tmp->next;
+      tmp->next = malloc(sizeof(*tmp->next));
+      tmp = tmp->next;
+      tmp->function = function;
+      tmp->argument = args;
+      pthread_mutex_unlock(&(p->lock));
+    }
+  else
+    {
+      p->first = malloc(sizeof(*p->first));
+      p->first->function = function;
+      p->first->argument = args;
+    }
+  p->tasks_to_do++;
+  pthread_mutex_unlock(&(p->lock));
+  if(pthread_cond_signal(&(p->condvar)) != 0)
+    {
+      write(2, "Error on condvar ThreadPool\n", strlen("Error on condvar ThreadPool\n"));
+      exit(-1);
+    }
+}
+
+void		thread_pool_init(t_thread_pool *p, unsigned int nb_threads)
+{
+  int		i;
+
+  i = 0;
+  p->first = NULL;
+  p->tasks_to_do = 0;
+  p->nb_workers = nb_threads;
+  // malloc the number of threads which we want in the threadpool
+  p->threads = malloc(nb_threads * sizeof(*p->threads));
+  // init mutex and cond var
+  if ((pthread_mutex_init(&(p->lock), NULL) != 0) ||
+      (pthread_cond_init(&(p->condvar), NULL) != 0) ||
+      p->first != NULL ||
+      p->threads == NULL)
+    {
+      write(2, "Error on init ThreadPool\n", strlen("Error on init ThreadPool\n"));
+      exit(-1);
+    }
+  // init and luch threads
+  while (i < nb_threads)
+    {
+      if (pthread_create(&(p->threads[i]), NULL, worker_start, (void*)p) != 0)
+	{
+	  write(2, "Error on init Thread workers\n", strlen("Error on init Thread workers\n"));
+	  exit(-1);
+	}      
+      i++;
+    }
+}
+
+void		thread_pool_destroy(t_thread_pool *p)
+{
+  int		i;
+
+  i = 0;
+  // wake up all threads
+  if (pthread_cond_broadcast(&(p->condvar)) != 0)
+    {
+      write(2, "Error pthread_cond_broadcast\n", strlen("Error pthread_cond_broadcast\n"));
+      exit(-1);
+    }
+  // destroy threads
+  usleep(1000);
+  while (i < p->nb_workers)
+    {
+      if (pthread_detach(p->threads[i]) != 0)
+	{
+	  write(2, "Error pthread_join\n", strlen("Error pthread_join\n"));
+	  exit(-1);
+	}
+      i++;
+    }
+  //  printf("Bye Bye\n");
+  // destroy condvar and mutex
+  pthread_mutex_destroy(&(p->lock));
+  pthread_cond_destroy(&(p->condvar));
+  // free the rest
+  thread_pool_free(p);
+}
+
+void		thread_pool_free(t_thread_pool *p)
+{
+  t_queue	*tmp;
+  
+  tmp = p->first;
+  // free queue
+  while (p->first != NULL)
+    {
+      tmp = p->first;
+      p->first = p->first->next;
+      free(tmp);
+    }
+  // free threads
+  free(p->threads);
+}
