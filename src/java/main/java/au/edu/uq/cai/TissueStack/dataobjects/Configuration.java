d599b133a083b0b2c3418e7a3ffe1f61fc810bbf
Adam Lin <adamph2004@yahoo.ca>
2012-05-22 15:58:00 +1000

    first persistence object for table configuration

---
+++ src/java/main/java/au/edu/uq/cai/TissueStack/dataobjects/Configuration.java
@@ -0,0 +1,41 @@
+package au.edu.uq.cai.TissueStack.dataobjects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="configuration")
+public class Configuration {
+	private String name;
+	private String value;
+	private String description;
+	
+	@Id
+	@Column(name="name")
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Column(name="value")
+	public String getValue() {
+		return value;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+	@Column(name="description")
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
