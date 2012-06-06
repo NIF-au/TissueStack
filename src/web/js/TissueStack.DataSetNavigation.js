64cece6196b0ef8d84ca1e2726814e0f63a5a082
Adam Lin <adamph2004@yahoo.ca>
2012-06-06 17:01:32 +1000

    tablet navigation menu

--- src/web/js/TissueStack.DataSetNavigation.js
+++ src/web/js/TissueStack.DataSetNavigation.js
@@ -21,7 +21,7 @@ TissueStack.DataSetNavigation.prototype = {
 				treeData[counter] = 
 					{title: dataSet.local_id + '@' +dataSet.host,
 						key: dataSet.id,
-						tooltip: (dataSet.description ? "Description: " + dataSet.description : "") 
+						tooltip: (dataSet.description ? "Description:m " + dataSet.description : "") 
 							+ "\nFile: " + dataSet.filename,
 						select: true,
 						expand: false
@@ -76,6 +76,25 @@ TissueStack.DataSetNavigation.prototype = {
 		  });
 	},
 	buildTabletMenu : function() {
-		// TODO: implement
+		var treeData = [];
+			if (TissueStack.dataSetStore.getSize() == 0) {
+				treeData[0] = {title: "No Data Sets Found", tooltip: "No Data Sets Found"};
+			} else {
+				var htmlString ="";	
+				for (var dataSetKey in TissueStack.dataSetStore.datasets) {
+					var dataSet = TissueStack.dataSetStore.datasets[dataSetKey];
+						htmlString += '<div data-role="collapsible" data-collapsed="false">' + '<h3>'+ dataSet.local_id + ' in ' + dataSet.host +'</h3>'+
+										'<p>'+ dataSet.description +'<br>'+ 'Location: '+ dataSet.filename +'</p>'+
+										'<fieldset data-role="controlgroup" data-mini="true">'+
+										'<input type="radio" name="radio-' + dataSet.local_id + '"'+' id="radio-'+ dataSet.local_id +'"'+' value="on" checked="checked"/>'+
+										'<label for="radio-'+ dataSet.local_id +'"'+'>ON</label>'+
+										'<input type="radio" name="radio-' + dataSet.local_id + '"'+' id="radio-off-'+ dataSet.local_id +'"'+' value="off" />'+
+										'<label for="radio-off-'+ dataSet.local_id + '"'+'>OFF</label>'+
+										'</fieldset></div>';
+																												
+				}
+				$('#tablet_tree').append(htmlString);
+				$("#tablet_tree").trigger("create");
+			}		
 	}
-};
+};		
