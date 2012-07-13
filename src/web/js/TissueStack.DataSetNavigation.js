3d418ce7b09068adb77be4414bfa9931581cb4a3
Harald Waxenegger <harald.waxenegger@gmail.com>
2012-07-13 11:46:25 +1000

    bugfixes for desktop data set selection

--- src/web/js/TissueStack.DataSetNavigation.js
+++ src/web/js/TissueStack.DataSetNavigation.js
@@ -206,7 +206,7 @@ TissueStack.DataSetNavigation.prototype = {
 		    		   node.parent.select(flag);
 		    		   return;
 		    	   }
-		    	   
+
 		    	   // now as for the action we need to take, evaluate whether we have a check/uncheck of a data set
 		    	   // but before receive key from node 
 		    	   var key = node.data.isFolder ? node.data.key : node.data.key.substring(0, node.data.key.length - "_base_layer".length);
@@ -214,14 +214,22 @@ TissueStack.DataSetNavigation.prototype = {
 		    		   // remove dataset in question
 		    		   _this.removeFromSelectedDataSetsByKey(key);
 		    	   }
-		    	   
+
 	    		   var selectedNodes = this.getSelectedNodes(true);
 	    		   // brief check
 	    		   if (selectedNodes.length > 2) {
 	    			   // we cannot display more than 2 data sets ... let the user know
 	    			   alert("Please deselect a data set before you select a different one");
 	    			   
-	    			   node.select(false);
+	    			   node._select(false, false, true);
+	    			   if (node.data.isFolder && node.childList) {
+			    		   for (var x=0;x<node.childList.length;x++) {
+			    			   node.childList[x]._select(false, false, true);
+			    		   }
+			    	   } else if (node.data.isBaseLayer) {
+			    		   // likewise if a base layer was selected/deselected, that results in the parent data set not being de/selected
+			    		   node.parent._select(false);
+			    	   }	    			   
 	    			   return;
 	    		   }
 
