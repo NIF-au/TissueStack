fb645ce6862cc4d130ba094a8cd41186a566c996
root <root@harald-build.(none)>
2013-03-21 21:22:22 +1100

    CentOS build: some bugfixes as well as improvements

--- BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -7,6 +7,25 @@ echo -e "***********************************************************************
 VERSION=1.1
 DATA_PATH=/mnt/tissuestack
 
+if [ ! -f /usr/include/gtk-2.0/gtk/gtk.h ]; then echo "ERROR: Could not find gtk development files. Insure the package is installed, e.g. run: 'yum install gtk2-devel"; exit -1
+fi
+
+javac &> /dev/null
+if [ $? -ne 0 ]; then
+	echo -e "\nYou have to install a java sdk, version >= 1.6. e.g 'yum install java-1.6.0-openjdk' might do the job !!!\n"
+	exit -1
+fi
+mvn -v &> /dev/null
+if [ $? -ne 0 ]; then
+	echo -e "\nYou have to install apache maven, version >= 3 !!!\n"
+	exit -1
+fi
+
+VERSION_3_LINE=`mvn -v | grep "Maven 3." | wc -l`
+if [ $VERSION_3_LINE -ne 1 ]; then 
+	echo -e "\n1111You have to install apache maven, version >= 3 !!!\n"; exit -1;
+fi
+
 echo -n "Cleaning..."
 make -f Makefile.centOS clean > /dev/null
 mvn clean > /dev/null
