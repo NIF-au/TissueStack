be228400098c40c338bddaa4f5ccc79e5beba0c6
Harald Waxenegger <harald.waxenegger@gmail.com>
2013-04-11 16:16:39 +1000

    more post-install modifications

--- BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 clear
 
 echo -e "\n\n*********************************************************************************************************"
@@ -7,53 +8,9 @@ echo -e "***********************************************************************
 VERSION=1.1
 DATA_PATH=/mnt/tissuestack
 
-if [ ! -f /usr/include/gtk-2.0/gtk/gtk.h ]; then echo "ERROR: Could not find gtk development files. Insure the package is installed, e.g. run: 'yum install gtk2-devel"; exit -1
-fi
-
-javac -version &> /tmp/java.version
-VERSION_1_6_LINE=`grep "1.6" /tmp/java.version | wc -l`
-if [ $VERSION_1_6_LINE -ne 1 ]; then
-	echo -e "\nYou have to install a java sdk, version >= 1.6. e.g 'yum install java-1.6.0-openjdk-devel' might do the job !!!\n"
+packaging/conf/tissuestack_modules.sh
+if [ $? -eq -1 ]; then 
 	exit -1
-fi
-mvn -v &> /dev/null
-if [ $? -ne 0 ]; then
-	echo -e "\nYou have to install apache maven, version >= 3 !!!\n"
-	exit -1
-fi
-
-VERSION_3_LINE=`mvn -v | grep "Maven 3." | wc -l`
-if [ $VERSION_3_LINE -ne 1 ]; then 
-	echo -e "\n1111You have to install apache maven, version >= 3 !!!\n"; exit -1;
-fi
-
-if [ ! -f /etc/profile.d/modules.sh ]; then echo "ERROR: Modules package is not installed !!!!!"; exit -1; fi;
-
-/etc/profile.d/modules.sh
-module load hdf5/1.8.5 2> /tmp/build_mod_load
-CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
-if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed hdf5 package (1.8.5). Please install it and load it: 'module load hdf5/1.8.5' !!!!!"; exit -1
-fi
-
-module load netcdf/4.1.1 2> /tmp/build_mod_load
-CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
-if [ $CHECK_SIZE -ne 0 ]; then
-        echo "ERROR: Could not locate needed netcdf package (4.1.1). Please install it and load it: 'module load netcdf/4.1.1' !!!!!"; exit -1
-fi
-
-module load nifticlib/2.0.0 2> /tmp/build_mod_load
-CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
-if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed nifti package (2.0.0). Please install it and load it: 'module load nifticlib/2.0.0' !!!!!"; exit -1
-fi
-
-module load minc/2.1.0 2> /tmp/build_mod_load
-CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
-if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed minc package (2.1.0). Please install it and load it: 'module load minc/2.1.0' !!!!!"; exit -1
-fi
-
-module load graphicsmagick/1.3.18 2> /tmp/build_mod_load
-CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
-if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed graphicsmagick package (1.3.18). Please install it and load it: 'module load graphicsmagick/1.3.18' !!!!!"; exit -1
 fi
 
 echo -n "Cleaning..."
---
+++ packaging/conf/tissuestack_modules.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo -n "Checking 'modules' requirements..."
+if [ ! -f /etc/profile.d/modules.sh ]; then echo "ERROR: Modules package is not installed !!!!!"; exit -1; fi;
+
+/etc/profile.d/modules.sh
+module load hdf5/1.8.5 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed hdf5 package (1.8.5). Please install it and load it: 'module load hdf5/1.8.5' !!!!!"; exit -1
+fi
+
+module load netcdf/4.1.1 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then
+        echo "ERROR: Could not locate needed netcdf package (4.1.1). Please install it and load it: 'module load netcdf/4.1.1' !!!!!"; exit -1
+fi
+
+module load nifticlib/2.0.0 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed nifti package (2.0.0). Please install it and load it: 'module load nifticlib/2.0.0' !!!!!"; exit -1
+fi
+
+module load minc/2.1.0 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed minc package (2.1.0). Please install it and load it: 'module load minc/2.1.0' !!!!!"; exit -1
+fi
+
+module load graphicsmagick/1.3.18 2> /tmp/build_mod_load
+CHECK_SIZE=`stat -c%s /tmp/build_mod_load`
+if [ $CHECK_SIZE -ne 0 ]; then echo "ERROR: Could not locate needed graphicsmagick package (1.3.18). Please install it and load it: 'module load graphicsmagick/1.3.18' !!!!!"; exit -1
+fi
+
+module load tissuestack 2> /dev/null
+exit 0
