44a9ff5d80560731b96ee9b83420620bcf20b2fc
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-09-30 07:31:00 +1000

    forgot about GPL

--- src/c++/common/TissueStackException.cpp
+++ src/c++/common/TissueStackException.cpp
@@ -1,14 +1,23 @@
+/*
+ * This file is part of TissueStack.
+ *
+ * TissueStack is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TissueStack is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with TissueStack.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include "exceptions.h"
 
 tissuestack::common::TissueStackException::TissueStackException() : _what("Exception not properly specified!") {}
 
-/*
-void tissuestack::common::TissueStackException::setWhat(const std::string what)
-{
-	this->_what = new char[what.length()+1];
-	std::strncpy(this->_what, what.c_str(), what.length());
-	this->_what[what.length()] = '\0';
-}*/
 tissuestack::common::TissueStackException::TissueStackException(const std::string what) : _what(what) {}
 
 const char * tissuestack::common::TissueStackException::what() const throw()
@@ -17,13 +26,4 @@ const char * tissuestack::common::TissueStackException::what() const throw()
 
 	return this->_what.c_str();
 }
-/*
-tissuestack::common::TissueStackException::~TissueStackException() throw()
-{
-	if (this->_what)
-	{
-		delete [] this->_what;
-		this->_what = nullptr;
-	}
-}*/
 
