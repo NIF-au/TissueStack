b5c1d2e75dad762ae460a6695e4581c84ea9c6a6
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-09-30 11:59:53 +1000

    centos packaging adaptions

--- BUILD_ME_LIKE_THIS_FOR_RPM.sh
+++ BUILD_ME_LIKE_THIS_FOR_RPM.sh
@@ -9,10 +9,22 @@ if [ $? -ne 0 ]; then
 	exit -1
 fi
 
-REDHAT_RPM_CONFIG=`yum list installed | grep "redhat-rpm-config" | wc -l`
+REDHAT_RPM_CONFIG=`yum list installed | grep "redhat-rpm-config" | wc -c`
 if [ $REDHAT_RPM_CONFIG -eq 0 ]; then
         echo "Install redhat-rpm-config first!"
         exit -1
+fi;
+
+IS_CENTOS_6_X=`cat /etc/*-release | grep -i "centos release 6."| wc -c`
+if [ $IS_CENTOS_6_X -gt 0 ]; then
+	CENTOS_DEVTOOLS_1_1=`yum list installed | grep "devtoolset-1.1" | wc -c`
+	if [ $CENTOS_DEVTOOLS_1_1 -eq 0 ]; then
+	        echo "Install devtools 1.1 first. CentOS 6.X needs it to compile C++ V11 code."
+	        echo "Execute: 'cd /etc/yum.repos.d;wget http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo';"
+	        echo "Then run: 'yum install devtoolset-1.1-gcc devtoolset-1.1-gcc-c++'"
+	        exit -1
+	fi;
+	scl enable devtoolset-1.1 bash
 fi;
 
 export TISSUESTACK_BUILD_VERSION=1.5
