e5816d43ca047d3e6d2373dcef9fc4717f6e9df7
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-09-30 11:12:06 +1000

    more packaging adjustments

--- BUILD_ME_LIKE_THIS_FOR_RPM.sh
+++ BUILD_ME_LIKE_THIS_FOR_RPM.sh
@@ -9,6 +9,12 @@ if [ $? -ne 0 ]; then
 	exit -1
 fi
 
+REDHAT_RPM_CONFIG=`yum list installed | grep "redhat-rpm-config" | wc -l`
+if [ $REDHAT_RPM_CONFIG -eq 0 ]; then
+        echo "Install redhat-rpm-config first!"
+        exit -1
+fi;
+
 export TISSUESTACK_BUILD_VERSION=1.5
 
 CURRENT_DIR=`pwd`
