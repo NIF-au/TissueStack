e9b4c602501aa00f5bdab8f8141925baecadc651
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-10-07 15:07:51 +1000

    add cvl specific packaging scripts

---
+++ packaging/post-uninstall-cvl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chkconfig --del tissuestack &>> /tmp/uninstall.log
+rm -rf /etc/init.d/tissuestack &>> /tmp/uninstall.log
+rm -rf /etc/httpd/conf.d/tissuestack.conf &>> /tmp/uninstall.log
+mv /etc/httpd/conf.d/welcome.conf.disabled /etc/httpd/conf.d/welcome.conf &>> /tmp/uninstall.log
+rm -rf /tmp/tissue_stack_communication &>> /tmp/uninstall.log
+service httpd restart &>> /tmp/uninstall.log
+/sbin/ldconfig
+exit 0
