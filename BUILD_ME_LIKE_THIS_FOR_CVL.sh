8743558048a6885f5e3c05a329f6ff31f0a911a7
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-10-07 14:23:15 +1000

    accidentally committed stuff meant for a different branch: undo it

--- BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -5,13 +5,18 @@ echo -e "\n\n*******************************************************************
 echo -e "!!! THIS BUILD OF TISSUE STACK BUILDS FOR THE PARTICULAR NEEDS OF CVL e.g MODULES AND README.TXT !!!"
 echo -e "*********************************************************************************************************\n\n"
 
-source packaging/conf/tissuestack_modules.sh
+VERSION=1.4
+#DATA_PATH=/mnt/tissuestack		
+DATA_PATH=/opt/tissuestack		
+
+source packaging/conf/tissuestack_modules.sh $VERSION
 
 echo -n "Cleaning..."
-make -f Makefile.cvl clean > /dev/null
+make -f Makefile.centOS clean > /dev/null
+mvn clean > /dev/null
 echo -e "Finished Cleaning.\n"
 
-make -f Makefile.cvl dist
+make -f Makefile.centOS dist VERSION=$VERSION DATA_PATH=$DATA_PATH
 if [ $? -ne 0 ]; then
 exit -1
 fi
