84cb6caf1be08bf79d162c4c7c8504cc69ee418a
Harald Waxenegger <harald.waxenegger@gmail.com>
2014-10-07 15:12:42 +1000

    some preliminary packaging checks

--- BUILD_ME_LIKE_THIS_FOR_CVL.sh
+++ BUILD_ME_LIKE_THIS_FOR_CVL.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 clear
 
+HAS_PQXX_LIB=`yum list installed | grep "libpqxx-devel" | wc -c`
+if [ $HAS_PQXX_LIB -eq 0 ]; then
+        echo "Build and install libpqxx first => see tissuestack git branch: libpqxx-devel!"
+        exit -1
+fi;
+
+IS_CENTOS_6_X=`cat /etc/*-release | grep -i "centos release 6."| wc -c`
+if [ $IS_CENTOS_6_X -gt 0 ]; then
+        CENTOS_DEVTOOLS_1_1=`yum list installed | grep "devtoolset-1.1" | wc -c`
+        if [ $CENTOS_DEVTOOLS_1_1 -eq 0 ]; then
+                echo "Install devtools 1.1 first. CentOS 6.X needs it to compile C++ V11 code."
+                echo "Execute: 'cd /etc/yum.repos.d;wget http://people.centos.org/tru/devtools-1.1/devtools-1.1.repo';"
+                echo "Then run: 'yum install devtoolset-1.1-gcc devtoolset-1.1-gcc-c++'"
+                exit -1
+        fi;
+        echo -e "\nIMPORTANT:\n"
+        echo "if the build fails with a message similar to these below, execute the line 'scl enable devtoolset-1.1 bash' prior to running this script"
+        echo "cc1plus: error: unrecognized command line option -std=c++11"
+        echo -e "cc1plus: error: unrecognized command line option -std=gnu++11\n"
+fi;
+
 echo -e "\n\n*********************************************************************************************************"
 echo -e "!!! THIS BUILD OF TISSUE STACK BUILDS FOR THE PARTICULAR NEEDS OF CVL e.g MODULES AND README.TXT !!!"
 echo -e "*********************************************************************************************************\n\n"
