fbc90335618a1cd028d6931943c7a1b1ae207820
Harald Waxenegger <harald.waxenegger@gmail.com>
2015-04-13 07:44:12 +1000

    dcmtk build for centos: empty for now

--- packaging/post-uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z "$2" ]; then
-	rm -rf /etc/init.d/tissuestack &>> /tmp/uninstall.log
-	cd /etc/init.d; update-rc.d -f tissuestack remove &>> /tmp/uninstall.log
-	a2dissite tissuestack.conf &>> /tmp/uninstall.log
-	a2ensite 000-default &>> /tmp/uninstall.log
-	/etc/init.d/apache2 restart &>> /tmp/uninstall.log
-fi
-exit 0
