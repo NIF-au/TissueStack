5788673c448b8c1848f439198f608761591e1d41
Harald Waxenegger <harald.waxenegger@gmail.com>
2015-04-13 11:06:06 +1000

    changes to BUILD_RPM

--- BUILD_RPM.sh
+++ BUILD_RPM.sh
@@ -1,8 +1,14 @@
-make distclean
+#!/bin/sh
+make distclean 2> /dev/null
 
-./configure --prefix=/tmp/dcmtk
+#./configure --prefix=/tmp/dcmtk
 
-replacement to affect build of shared libs
+if [ ! -f config/Makefile.def ]; then
+	echo "Configure must have failed. There is no config/Makefile.def"
+	exit 1
+fi
+
+#replacement to affect build of shared libs
 sed -i "/^AR[ ]*=/c\AR = gcc " config/Makefile.def
 sed -i "/^ARFLAGS[ ]*=/c\ARFLAGS = -shared -o " config/Makefile.def
 sed -i "/^LIBEXT[ ]*=/c\LIBEXT = so " config/Makefile.def
@@ -11,3 +17,7 @@ sed -i "/^CXXFLAGS[ ]*=/s/$/ -fPIC/" config/Makefile.def
 sed -i "/^CFLAGS[ ]*=/s/$/ -fPIC/" config/Makefile.def
 
 make install && make install-lib
+if [ $? -ne 0 ]; then
+	echo "make/install failed" 
+	exit 1
+fi
