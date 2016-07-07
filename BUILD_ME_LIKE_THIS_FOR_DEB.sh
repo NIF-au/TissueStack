3260ca2a9f6fa639dddc0529de43de3fb91ca4ef
tissuestack <tissuestack@harald.localdomain>
2016-07-07 17:03:39 +1000

    deb build changes

--- BUILD_ME_LIKE_THIS_FOR_DEB.sh
+++ BUILD_ME_LIKE_THIS_FOR_DEB.sh
@@ -4,18 +4,22 @@ clear
 export IS_RELEASE=1
 export TISSUESTACK_BUILD_VERSION=2.2
 
+echo "Setting up build directory and copying config files"
+BUILD_DIR=/tmp/tissuestack_build_debian
+rm -rf $BUILD_DIR
+
+EXT_BUILD_DIR=$BUILD_DIR/tissuestack-$TISSUESTACK_BUILD_VERSION
+
 TARGET=server
-if [ $# -ne 0 ] && [ "$1" == "CLIENTS" ]; then
+if [ $# -ne 0 ] && [ "$1" = "CLIENTS" ]; then
     TARGET=clients
+    EXT_BUILD_DIR=$BUILD_DIR/tissuestack-$TARGET-$TISSUESTACK_BUILD_VERSION
 fi;
 
 CURRENT_DIR=`pwd`
 
-echo "Setting up build directory and copying config files"
-BUILD_DIR=/tmp/tissuestack_build_debian
-rm -rf $BUILD_DIR
-mkdir -p $BUILD_DIR/tissuestack-$TISSUESTACK_BUILD_VERSION/debian
-cp -rf $CURRENT_DIR/deb/$TARGET/* $BUILD_DIR/tissuestack-$TISSUESTACK_BUILD_VERSION/debian
+mkdir -p $EXT_BUILD_DIR/debian
+cp -rf $CURRENT_DIR/deb/$TARGET/* $EXT_BUILD_DIR/debian
 
 echo "Calling TissueStack make with target $TARGET"
 cd $CURRENT_DIR/src/c++;make $TARGET
@@ -24,5 +28,5 @@ echo "Believe it or not we have to wait for tar to finish off..."
 sleep 5
 
 echo "Starting DEBIAN package build now ..."
-cd $BUILD_DIR/tissuestack-$TISSUESTACK_BUILD_VERSION
+cd $EXT_BUILD_DIR
 dpkg-buildpackage -us -uc
