732f93f5a8ac686670a676484b8b10f465a4ee8b
aswinnarayanan <aswinn89@gmail.com>
2022-11-08 07:23:05 +0000

    Add db initialise script for tissuestack Needs to be run manually for postgres 8.4.x

---
+++ tissuestack-db.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -U postgres -a -f /sql/create_tissuestack_db.sql
+psql -U tissuestack -d tissuestack -a -f /sql/create_tissuestack_tables.sql
+psql -U tissuestack -d tissuestack -a -f /sql/create_tissuestack_config.sql
+# psql -U tissuestack -d tissuestack -a -f /sql/patches.sql
